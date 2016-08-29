858ccecbe01c6b4ac6ca14a50390336821432cf7
Harrison Valetski <sonnyfromtacoma@msn.com>
2016-08-29 10:33:22 -0400

    Create multiple_checkbox.php

---
+++ multiple_checkbox.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Tutorial</title>
+</head>
+<body>
+<form action="php_multiple_checkbox2.php" method="post" name="form1">
+Please select color.<br>
+<input type="checkbox" name="chkColor[]" value="Red">Red<br>
+<input type="checkbox" name="chkColor[]" value="Blue">Blue<br>
+<input type="checkbox" name="chkColor[]" value="Green">Green<br>
+<input type="checkbox" name="chkColor[]" value="Orange">Orange<br>
+<input type="checkbox" name="chkColor[]" value="Black">Black<br>
+<input name="btnSubmit" type="submit" value="Submit">
+</form>
+</body>
+</html>
